7a60c7852ee195e202cfd52bf85617bb7a7ea144
meghpatil007 <meghpatil48@gmail.com>
2022-08-09 18:55:12 +0530

    added win lose tie

--- flip coin simulation.sh
+++ flip coin simulation.sh
@@ -1,14 +1,33 @@
 echo "This problem displays the winner head or tails"
 echo
+#head_count
+hc=0
 
-for((i=1; i<=10; i++))
+#tail_count
+tc=0
+
+
+while((hc!=21 && tc!=21))
 do
 	x=$((RANDOM%2))
 
 	if((x==0))
 	then
         	echo "Heads"
+		((hc++))
 	else
         	echo "Tails"
+		((tc++))
 	fi
 done
+echo
+echo "head won = " $hc
+echo "tail won = " $tc
+echo
+	if((hc>tc))
+	then echo "heads win by " $((hc-tc))
+	elif((hc<tc))
+	then echo "tails win by " $((tc-hc))
+	elif((hc==tc))
+	then echo "its tie"
+	fi
